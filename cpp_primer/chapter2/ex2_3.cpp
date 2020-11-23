968a5480f32aee1a11287ebaa134dae6e24f5c01
SillyPasty <SillyPasty@users.noreply.github.com>
2020-11-23 10:31:32 +0800

    cpp_primer_ch2

---
+++ cpp_primer/chapter2/ex2_3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    unsigned u = 10, u2 = 42;
+    std::cout << u - u2 << std::endl;
+    int a = -1;
+    unsigned b = 1;
+    std::cout << a * b << std::endl;
+    return 0;
+}
