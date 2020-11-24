abf5064793e581e4dde16f7bf7d2c0c8f8e52015
SillyPasty <1179041704@qq.com>
2020-11-24 15:44:19 +0800

    chapter2_ex

---
+++ cpp_primer/chapter2/ex2_17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+    int val = 1;
+    int &ref_val = val;
+    std::cout << val << " " << ref_val << std::endl;
+    ref_val += 1;
+
+    int new_val = 2;
+    ref_val = new_val;
+    ref_val += 20;
+    std::cout << new_val << std::endl;
+    int &ref_ref_val = ref_val;
+    ref_ref_val++;
+    
+    std::cout << val << " " << ref_val << std::endl;
+    return 0;
+}
---
+++ cpp_primer/chapter2/ex2_21.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    int i = 0;
+    double *dp = nullptr;
+    // std::cout << *dp << std::endl;
+    // int *ip = i;
+    int *p = &i;
+
+}
