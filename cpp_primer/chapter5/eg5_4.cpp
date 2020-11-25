28c57ada57723118f02c0648d51186b147eadd11
SillyPasty <1179041704@qq.com>
2020-11-25 16:26:42 +0800

    chapter5 eg

---
+++ cpp_primer/chapter5/eg5_4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<cstdio>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+int main()
+{
+    vector<int> vec;
+    int i;
+    while(cin >> i)
+        vec.push_back(i);
+    auto end = vec.end();
+    for (auto beg = vec.begin(); beg != vec.end(); ++beg) {
+        cout << *beg << endl;
+        // printf("%p")
+        // vec.push_back(*beg);   // very danger!!!
+    }
+    return 0;
+}
---
+++ cpp_primer/chapter5/eg5_6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int i;
+    cin >> i;
+    try
+    {
+        if (i == 0) {
+            throw runtime_error("WTF!");
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
